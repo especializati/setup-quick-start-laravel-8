4b35764fcaf78767a7845931dc30df2c58ed178a
Carlos Ferreira <conta@especializati.com.br>
2021-10-13 15:22:12 -0300

    Install AdminLTE 3 and configure

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Example Routes Admin
+Route::prefix('admin')
+        //->middleware(['auth'])
+        ->as('admin.')
+        ->group(function () {
+            Route::view('/', 'admin.pages.home.index')->name('home');
+        });
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
